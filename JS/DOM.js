5f26fdd3c3f51fbf1776c1cec23e8e46ca43f6ec
Peter <Peter@laytonenterprises.com>
2018-08-06 09:34:21 -0600

    The Elder Scrolls

---
+++ JS/DOM.js
@@ -0,0 +1,12 @@
+// DOM -> Document Object Model
+
+const user = {
+    name: 'Peter',
+    email: 'peter@laytonenterprises.com',
+    favorites: [
+        'Chipotle',
+        'Chik fil a'
+    ]
+}
+
+user.favorites[1];
---
+++ JS/index.js
@@ -0,0 +1,9 @@
+// DOM -> Document Object Model
+
+const user = {
+    name: 'Peter',
+    email: 'peter@laytonenterprises.com',
+    favorites: [
+        
+    ]
+}
